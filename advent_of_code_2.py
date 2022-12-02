ed74086205ffb18009ebf0c27cf662745b6f2403
Lucas G-R <lucasgether@gmail.com>
2022-12-02 20:52:52 +0100

    Make it prettier

--- advent_of_code_2.py
+++ advent_of_code_2.py
@@ -1,45 +1,32 @@
 def method():
     file = open('input2.txt', 'r')
-    sum = 0
-    # for line in file:
-    #     opponent = find_move_opponent(line.split()[0])
-    #     you = find_move_you(line.split()[1])
-    #     sum += find_score(opponent=opponent, you=you)
+    sum1 = 0
+    sum2 = 0
     for line in file:
-        opponent = find_move_opponent(line.split()[0])
-        you = find_move_you_part2(opponent=opponent, you=line.split()[1])
-        sum += you #find_score(opponent=opponent, you=you)
-    print(sum)
+        opponent = find_move(line.split()[0])
+        you = find_move(line.split()[1])
+        sum1 += find_score(opponent=opponent, you=you)
+        you_new_strat = find_move_strategy(opponent=opponent, you=line.split()[1])
+        sum2 += you_new_strat 
+    print(f"Part 1 - Sum: {sum1}")
+    print(f"Part 2 - Sum: {sum2}")
 
-def find_move_opponent(input: str):
-    match input:
-        case "A":
-            return 1
-        case "B":
-            return 2
-        case "C":
-            return 3
+def find_move(input: str):
+    if input in ["A", "X"]:
+        return 1
+    elif input in ["B", "Y"]:
+        return 2
+    else:
+        return 3
 
-def find_move_you(input: str):
-    match input:
-        case "X":
-            return 1
-        case "Y":
-            return 2
-        case "Z":
-            return 3
-
-def find_move_you_part2(opponent: int, you: str):
+def find_move_strategy(opponent: int, you: str):
     match you:
         case "X": # lose
-            move = 1 + ((opponent + 1) % 3)
-            return move
+            return 1 + ((opponent + 1) % 3)
         case "Y": # draw
-            move = opponent
-            return move + 3
+            return opponent + 3
         case "Z": # win
-            move = 1 + (opponent % 3)
-            return move + 6
+            return 7 + (opponent % 3)
 
 def find_score(opponent: int, you: int):
     if opponent == you:
@@ -48,7 +35,6 @@ def find_score(opponent: int, you: int):
         return you # return a loss (zero) plus the value of your move
     else:
         return 6 + you # return a win (six) plus the value of your move
-
 
 if __name__ == '__main__':
     method()
