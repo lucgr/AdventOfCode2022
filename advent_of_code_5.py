eba82d99eab3ce6a1ea176f785912042a74d6736
Lucas G-R <36277820+lucgr@users.noreply.github.com>
2022-12-05 10:12:45 +0100

    First start day 5

--- advent_of_code_5.py
+++ advent_of_code_5.py
@@ -1,8 +1,15 @@
 def method():
     file = open("input/input5.txt", "r")
+    stacks = []
     for line in file:
         ...
 
+def process_instruction(line: str);
+    input = line.split(' ')
+    move_amount = input[1]
+    move_from = input[3]
+    move_to = input[5]
+    
 
 if __name__ == "__main__":
     method()
